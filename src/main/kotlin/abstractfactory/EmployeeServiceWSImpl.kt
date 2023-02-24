aafa4521ebb1a70aa5e78eef1572bc5d8dab933b
Esther <esther.mejia.antonio035@gmail.com>
2023-02-23 22:07:14 -0600

    add all1

--- src/main/kotlin/abstractfactory/EmployeeServiceWSImpl.kt
+++ src/main/kotlin/abstractfactory/EmployeeServiceWSImpl.kt
@@ -1,6 +1,7 @@
 package abstractfactory
 
 class EmployeeServiceWSImpl:IEmployeeService {
+    //comentario 23/02/2013
     private val employees = listOf("SOA-Maria", "SOA-Rosalia", "SOA-Liliana")
     override fun getEmployee(): List<String> {
         return this.employees
